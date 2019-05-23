dd2c7d5352b5011fd403a86ca16d17e8d624306d
Ahti Kukk <ahti.kukk>
2019-05-23 10:17:22 +0300

    Use Twig for templating, create a url for creating bookings

--- web/index.php
+++ web/index.php
@@ -1,9 +1,22 @@
 <?php
-// web/index.php
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-// ... definitions
+$app['debug'] = true;
+
+$app->get('/hello', function () {
+    return 'Hello world!';
+});
+
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+    'twig.path' => __DIR__.'/../views',
+]);
+
+$app->get('/bookings/create', function () use ($app) {
+    return $app['twig']->render('base.html.twig');
+});
+
 
 $app->run();
